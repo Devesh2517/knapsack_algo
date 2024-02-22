01e81e5a97e9b6042ba0d4bc850e7541d7894d7a
Devesh2517 <prajapatidevesh205@gmail.com>
2024-02-23 00:16:00 +0530

    item app created and the two api created

---
+++ item/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import *
+from .models import *
+from .helpers import custom_response
+from rest_framework import status
+from drf_spectacular.utils import extend_schema
+
+# Create your views here.
+
+
+class ItemView(APIView):
+    serializer_class = ItemSerializer
+   
+    def get(self,request):
+        db_items = Item.objects.all()
+        try:
+            serializer_items = ItemSerializer(db_items, many=True)
+            return custom_response(serializer_items.data,"successfull",status.HTTP_200_OK)
+        except Exception as Error:
+            return custom_response(str(Error),"Unsucessfull",status.HTTP_500_INTERNAL_SERVER_ERROR)
+    @extend_schema(
+        request=ItemSerializer,
+        responses={
+            status.HTTP_201_CREATED: {
+                                        "example": [
+                                            {
+                                            "item_id": 1,
+                                            "name": "Iphone15",
+                                            "price": "100000.00",
+                                            "description": "Mobile of apple company 256GB rom ,16GB ram"
+                                            },
+                                            {
+                                            "item_id": 2,
+                                            "name": "MI",
+                                            "price": "10000.00",
+                                            "description": ""
+                                            }
+                                        ],
+                                        "message": "successfull"
+                                        },
+            status.HTTP_400_BAD_REQUEST: {"example": {"error": "Bad request"}}
+        }
+    )   
+    def post(self, request):
+        try:
+            serializer = ItemSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return custom_response(serializer.data, "successfull",status.HTTP_200_OK)
+        except Exception as Error:
+            return custom_response(str(Error), "Unsuccessfull",status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
